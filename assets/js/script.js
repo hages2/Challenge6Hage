b174e95a50e9808d777b5a7e54c31e42b01532ce
Stephanie Hage <stephaniehage23@gmail.com>
2021-07-04 10:25:40 -0400

    style and format

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,12 +32,12 @@ function getGeoLocation(searchTerm) {
         .then(function (response) {
             var lat = response[0].lat;
             var lon = response[0].lon;
-            getForecast(lat, lon);
+            getForecast(lat, lon, searchTerm);
         });
 
 }
 
-function getForecast(lat, lon) {
+function getForecast(lat, lon, searchTerm) {
     fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" +
             lat + "&lon=" + lon + "&units=imperial&appid=6ccdfe86a41e47db6bb4150f78fc2f74"
         )
@@ -46,47 +46,119 @@ function getForecast(lat, lon) {
         })
         .then(function (response) {
             render5DayForecast(response.daily)
+            renderCurrentWeather(response.daily[0], searchTerm)
         })
 
     //EMPTY OUT ALL THE WEATHER DATA ON SCREEN
     //AND REPRINT WITH THE NEW STUFF
 }
 
+function renderCurrentWeather(weather, city) {
+    console.log(weather)
+    var currentWeatherDiv = $("#current-weather-container");
+    currentWeatherDiv.empty();
+    var headerSpan = $("<span>");
+    headerSpan.addClass("card-header")
+    headerSpan.attr('id',"city-from-search");
+    headerSpan.text(city);
+
+    var iconImg = $("<img>");
+    var iconCode = weather.weather[0].icon
+    iconImg.attr("src","http://openweathermap.org/img/w/" + iconCode + ".png");
+    iconImg.attr("width", "40px");
+    headerSpan.append(iconImg);
+
+
+    currentWeatherDiv.append(headerSpan);
+
+    var dateSpan = $("<span>");               
+    dateSpan.text(moment.unix(weather.dt).format('MM/DD/YYYY'));
+    currentWeatherDiv.append(dateSpan);
+
+    
+
+    var tempSpan = $("<span>");
+    tempSpan.text("Temp: " + weather.temp.day + " F");
+    currentWeatherDiv.append(tempSpan);
+
+    var windSpan = $("<span>");
+    windSpan.text("Wind: " + weather.wind_speed + " MPH");
+    currentWeatherDiv.append(windSpan);
+
+    var uviParentSpan = $("<span>");
+    uviParentSpan.text("UV Index: " );
+    var uviChildSpan = $("<span>");
+    uviChildSpan.text(weather.uvi)
+    if(weather.uvi < 3){
+        uviChildSpan.css('background-color', 'green');
+    }
+    if(3 > weather.uvi < 8){
+        uviChildSpan.css('background-color', 'yellow');
+    }
+    if(weather.uvi >= 8){
+        uviChildSpan.css('background-color', 'red');
+    }
+    uviParentSpan.append(uviChildSpan);
+    currentWeatherDiv.append(uviParentSpan);
+}
+
 function render5DayForecast(days) {
-    console.log(days)
+    
+    var forecastContainerDiv = $("#forecast-container-div");
+    forecastContainerDiv.empty();
+
+    var headerDiv = $("<h2>")
+    headerDiv.addClass("card-header")
+    headerDiv.attr('id',"5-day-forecast");
+    headerDiv.text("5-Day Forecast:");
+    forecastContainerDiv.append(headerDiv);
+
     for(var i=0; i<5; ++i){
         $("#day-"+i).empty();
-
+        
         var dayForecastDiv = $("<div>");
-        var dayForecastUL = $("<ul>");
+        dayForecastDiv.addClass("forecast-card card col-md-2")
+        dayForecastDiv.attr('id',"day-"+i);
+        
+        var dateSpan = $("<span>");     
+        console.log(days[i].dt)           
+        dateSpan.text(moment.unix(days[i].dt).format('MM/DD/YYYY'));
+        dayForecastDiv.append(dateSpan);
 
-        var tempLI = $("<li>");
-        tempLI.text(days[i].temp.day);
-        dayForecastUL.append(tempLI);
+        var iconImg = $("<img>");
+        var iconCode = days[i].weather[0].icon
+        iconImg.attr("src","http://openweathermap.org/img/w/" + iconCode + ".png");
+        iconImg.attr("width", "40px");
+        dayForecastDiv.append(iconImg);
 
-        var windLI = $("<li>");
-        windLI.text(days[i].wind_speed);
-        dayForecastUL.append(windLI);
+        var tempSpan = $("<span>");
+        tempSpan.text("Temp: " + days[i].temp.day + " F");
+        dayForecastDiv.append(tempSpan);
 
-        var humidityLI = $("<li>");
-        humidityLI.text(days[i].humidity);
-        dayForecastUL.append(humidityLI);
+        var windSpan = $("<span>");
+        windSpan.text("Wind: " + days[i].wind_speed + " MPH");
+        dayForecastDiv.append(windSpan);
 
-        dayForecastDiv.append(dayForecastUL)
-        $("#day-"+i).append(dayForecastDiv)
+        var humiditySpan = $("<span>");
+        humiditySpan.text("Humidity: " + days[i].humidity + "%");
+        dayForecastDiv.append(humiditySpan);
+
+
+        forecastContainerDiv.append(dayForecastDiv)
+        //$("#day-"+i).append(dayForecastDiv)
     }
 };
 
 function renderHistoryToList(){
     var cities = JSON.parse(localStorage.getItem("cityHistory"));
-    var cityListUL = $("#city-list");
+    var cityListUL = $("#city-list").addClass("city-ul");
     cityListUL.empty();
 
     for(var i =0; i < cities.length; ++i){
         var city = $("<li>");
         //attach on click method to EACH list item
         city.on( "click", handleHistoryClick )
-        var innerText = $("<p>").text(cities[i]);
+        var innerText = $("<p>").text(cities[i]).addClass("list-group-item list-group-item-secondary d-flex justify-content-between align-items-center");
         city.append(innerText);
     
 
